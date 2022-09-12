dba5ecd60fe919f069ff0e4189147a56b65e2514
josieyan123 <josieyan123@gmail.com>
2022-09-12 15:49:35 +0800

    finish and pass all test in CollectionOperator

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,31 +1,82 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.List;
+import java.util.*;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        if(left < right) {
+            while(left <= right){
+                resultList.add(left);
+                left += 1;
+            }
+        } else {
+            while(left >= right) {
+                resultList.add(left);
+                left -= 1;
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        if(left < right && left % 2 == 0){
+            while(left <= right) {
+                resultList.add(left);
+                left += 2;
+            }
+        }
+        else if(left < right && left % 2 != 0){
+            left += 1;
+            while(left <= right) {
+                resultList.add(left);
+                left += 2;
+            }
+        }
+        else if (left > right && left % 2 == 0){
+            while(left >= right) {
+                resultList.add(left);
+                left -= 2;
+            }
+        }
+        else{
+            left -= 1;
+            while(left >= right) {
+                resultList.add(left);
+                left -= 2;
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (int i : array) {
+            if(i % 2 == 0){
+                resultList.add(i);
+            }
+        }
+        return resultList;
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        var lastIndex = array.length - 1;
+        return array[lastIndex];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (int i : firstArray) {
+            if(Arrays.asList(secondArray).contains(i)){
+                resultList.add(i);
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
+
